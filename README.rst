40dbdb04fb7c86b4b66934ff43d41f1c9de8e1f4
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-07-15 17:42:33 -0500

    Let the fun begin

---
+++ README.rst
@@ -0,0 +1,4 @@
+funfactory is the what makes `playdoh`_ fun. You import it within a Django `manage.py`_ file but the playdoh docs will explain everything.
+
+.. _`playdoh`: https://github.com/mozilla/playdoh
+.. _`manage.py`: https://github.com/mozilla/playdoh/blob/base/manage.py
---
+++ setup.py
@@ -0,0 +1,27 @@
+extra_setup = {}
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+else:
+    extra_setup.update(dict(packages=find_packages(exclude=['ez_setup']),
+                            install_requires=[]))
+
+setup(
+    name='funfactory',
+    version='1.0',
+    description="Mozilla's Django app skeleton.",
+    long_description=open('README.rst').read(),
+    author='Kumar McMillan and contributors',
+    author_email='',
+    license="Mozilla License",
+    url='http://farmdev.com/projects/fudge/',
+    include_package_data=True,
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        ],
+    **extra_setup
+    )
