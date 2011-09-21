8e3284739519bf05313a599799fa3928abc93344
Dave Dash <dd+github@davedash.com>
2011-09-21 16:50:42 -0700

    Better absolutification.

--- funfactory/utils.py
+++ funfactory/utils.py
@@ -1,12 +1,22 @@
+import logging
+
 from django.conf import settings
+
+log = logging.getLogger('funfactory')
 
 
 def absolutify(url):
     """Takes a URL and prepends the SITE_URL"""
-    protocol = settings.PROTOCOL
-    hostname = settings.DOMAIN
-    port = settings.PORT
-    if (protocol, port) in (('https://', 443), ('http://', 80)):
-        return ''.join(map(str, (protocol, hostname, url)))
-    else:
-        return ''.join(map(str, (protocol, hostname, ':', port, url)))
+    site_url = getattr(settings, 'SITE_URL', False)
+
+    # If we don't define it explicitly
+    if not site_url:
+        protocol = settings.PROTOCOL
+        hostname = settings.DOMAIN
+        port = settings.PORT
+        if (protocol, port) in (('https://', 443), ('http://', 80)):
+            site_url = ''.join(map(str, (protocol, hostname)))
+        else:
+            site_url = ''.join(map(str, (protocol, hostname, ':', port)))
+
+    return site_url + url
