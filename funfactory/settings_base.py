55efbe5944304c813e6ef82974b877649cb8d24b
Austin King <shout@ozten.com>
2011-09-27 15:26:00 -0700

    We should reference PROD_LANGUAGES through settings to allow overriding fixes Bug#689763

--- funfactory/settings_base.py
+++ funfactory/settings_base.py
@@ -91,7 +91,7 @@ LANGUAGE_URL_MAP = lazy(lazy_lang_url_map, dict)()
 def lazy_langs():
     from django.conf import settings
     from product_details import product_details
-    langs = DEV_LANGUAGES if settings.DEV else PROD_LANGUAGES
+    langs = DEV_LANGUAGES if settings.DEV else settings.PROD_LANGUAGES
     return dict([(lang.lower(), product_details.languages[lang]['native'])
                  for lang in langs])
 
