7cb1b174283f64ee19026602d1ca1468d75381c7
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-12-20 15:56:25 -0600

    Removing unnecessary tests for dev warnings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,5 +1,3 @@
-import warnings
-
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
@@ -7,22 +5,6 @@ from mock import Mock, patch
 from nose.tools import raises
 
 from funfactory.manage import validate_settings
-
-
-def setup():
-    warnings.filterwarnings('error')
-
-
-def teardown():
-    warnings.resetwarnings()
-
-
-@raises(UserWarning)
-@patch.object(settings, 'DEBUG', True)
-@patch.object(settings, 'SECRET_KEY', '')
-@patch.object(settings, 'SESSION_COOKIE_SECURE', True)
-def test_empty_secret_key_for_dev():
-    validate_settings(settings)
 
 
 @patch.object(settings, 'DEBUG', True)
