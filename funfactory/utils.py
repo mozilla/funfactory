164456ff654097bf2181e7a7404a1b1828e70bce
Ryan Freebern <ryan@freebern.org>
2012-05-19 14:42:45 -0400

    Add settings management util functions

--- funfactory/utils.py
+++ funfactory/utils.py
@@ -2,6 +2,8 @@ import logging
 
 from django.conf import settings
 
+import settings_base
+
 log = logging.getLogger('funfactory')
 
 
@@ -20,3 +22,31 @@ def absolutify(url):
             site_url = ''.join(map(str, (protocol, hostname, ':', port)))
 
     return site_url + url
+
+
+def get_middleware(exclude):
+    """
+    Returns the default funfactory MIDDLEWARE_CLASSES list without the
+    middlewares listed in exclude.
+    """
+
+    return filter(lambda m: m not in exclude, settings_base.MIDDLEWARE_CLASSES)
+
+
+def get_apps(exclude):
+    """
+    Returns the default funfactory INSTALLED_APPS list without the apps
+    listed in exclude.
+    """
+
+    return filter(lambda a: a not in exclude, settings_base.INSTALLED_APPS)
+
+
+def get_template_context_processors(exclude):
+    """
+    Returns the default funfactory TEMPLATE_CONTEXT_PROCESSORS without the
+    processors listed in exclude.
+    """
+
+    return filter(lambda p: p not in exclude,
+        settings_base.TEMPLATE_CONTEXT_PROCESSORS)
--- tests/test__utils.py
+++ tests/test__utils.py
@@ -4,7 +4,7 @@ from mock import patch
 from nose.tools import eq_
 from django.test import TestCase
 
-from funfactory.utils import absolutify
+import funfactory.utils as utils
 
 
 @patch.object(settings, 'DOMAIN', 'test.mo.com')
@@ -12,16 +12,34 @@ class AbsolutifyTests(TestCase):
     ABS_PATH = '/some/absolute/path'
 
     def test_basic(self):
-        url = absolutify(AbsolutifyTests.ABS_PATH)
+        url = utils.absolutify(AbsolutifyTests.ABS_PATH)
         eq_('http://test.mo.com/some/absolute/path', url)
 
     @patch.object(settings, 'PROTOCOL', 'https://')
     @patch.object(settings, 'PORT', 443)
     def test_https(self):
-        url = absolutify(AbsolutifyTests.ABS_PATH)
+        url = utils.absolutify(AbsolutifyTests.ABS_PATH)
         eq_('https://test.mo.com/some/absolute/path', url)
 
     @patch.object(settings, 'PORT', 8000)
     def test_with_port(self):
-        url = absolutify(AbsolutifyTests.ABS_PATH)
+        url = utils.absolutify(AbsolutifyTests.ABS_PATH)
         eq_('http://test.mo.com:8000/some/absolute/path', url)
+
+
+@patch.object(utils.settings_base, 'INSTALLED_APPS', ('one', 'two', 'three'))
+class GetAppsTests(TestCase):
+    def test_exclude(self):
+        eq_(utils.get_apps(exclude=['three']), ('one', 'two'))
+
+
+@patch.object(utils.settings_base, 'MIDDLEWARE_CLASSES', ('foo', 'bar', 'baz'))
+class GetMiddlewareTests(TestCase):
+    def test_exclude(self):
+        eq_(utils.get_middleware(exclude=['foo']), ('bar', 'baz'))
+
+
+@patch.object(utils.settings_base, 'TEMPLATE_CONTEXT_PROCESSORS', ('a', 'b'))
+class GetTemplateContextProcessorsTests(TestCase):
+    def test_exclude(self):
+        eq_(utils.get_template_context_processors(exclude=['a']), ('b',))
