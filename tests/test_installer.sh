8f2f3c6f160b2daaa89f3c1cce7684bc74d7f8c9
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-06-07 12:25:55 -0500

    Adjust rel path of funfactory installer

--- tests/test_installer.sh
+++ tests/test_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This is a smoke test to check the installer from Jenkins CI.
 rm -fr installer_test
-./.tox/bin/py26/bin/funfactory --no-input --pkg installer_test
+../.tox/bin/py26/bin/funfactory --no-input --pkg installer_test
 exit $?
